2b81bd701bd7ee73d5fdcd550cafdc45a9fdfbe6
Ageng Setyo Nugroho <setyo.nugroho96@gmail.com>
2021-11-04 09:44:22 +0700

    [Kecermatan] Create Service Kejiwaan Group - Create Service Create / Update - Create Service Delete - Create Service Find All - Create Service Activation

---
+++ src/Controller/Group/Kejiwaan/index.ts
@@ -0,0 +1,96 @@
+import { Request, Response, NextFunction } from "express";
+import Responses from "../../../Utils/Helper/Response";
+import KejiwaanService from "../../../Service/Group/Kejiwaan";
+import { PayloadCreateKejiwaanVO, PayloadActivationVO } from "../../../Types";
+import { v4 as uuidv4 } from "uuid";
+
+class Kejiwaan {
+    kejiwaanService: KejiwaanService;
+
+    constructor() {
+        this.kejiwaanService = new KejiwaanService();
+    }
+
+    public async insertData(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            if (!req.body?.title) throw "Title Required";
+            else if (!req.body?.description) throw "Description Required";
+            else if (!req.body?.time) throw "Time Required";
+            else if (req.body?.is_active == null) throw "Bad Request";
+
+        } catch (error) {
+            return Responses.badRequest(res, error, next);
+        }
+
+        try {
+            let Result: any;
+            const payload: PayloadCreateKejiwaanVO = {
+                secureId: "",
+                title: req.body?.title,
+                description: req.body?.description,
+                time: req.body?.time,
+                is_active: req.body?.is_active,
+            }
+
+            if (!req.body?.secureId) {
+                payload.secureId = uuidv4();
+                Result = await this.kejiwaanService.createData(payload);
+            } else {
+                payload.secureId = req.body?.secureId;
+                Result = await this.kejiwaanService.updateData(payload)
+            }
+
+            return Responses.success(res, Result)
+        } catch (error) {
+            return Responses.failed(res, error, next);
+        }
+    }
+
+    public async activation(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            if (!req.body?.secureId) throw "Secure Id Required"
+            else if (req.body?.is_active == null) throw "Bad Request"
+        } catch (error) {
+            return Responses.badRequest(res, error, next);
+        }
+
+        try {
+            const payload: PayloadActivationVO = {
+                secureId: req.body.secureId,
+                is_active: req.body.is_active,
+            }
+
+            const Result = await this.kejiwaanService.activation(payload);
+            return Responses.success(res, Result)
+        } catch (error) {
+            return Responses.failed(res, error, next);
+        }
+    }
+
+    public async findAll(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            const Result = await this.kejiwaanService.findAll();
+            return Responses.success(res, Result);
+        } catch (error) {
+            return Responses.failed(res, error, next);
+        }
+    }
+
+    public async deleteData(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            if (!req.params?.secureId) throw "Bad Request";
+        } catch (error) {
+            return Responses.badRequest(res, error, next);
+        }
+
+        try {
+            const payload: string = req.params.secureId;
+            const Result = await this.kejiwaanService.deleteData(payload);
+            return Responses.success(res, Result);
+        } catch (error) {
+            return Responses.failed(res, error, next);
+        }
+    }
+}
+
+export default Kejiwaan;
---
+++ src/Model/Group/Kejiwaan/index.d.ts
@@ -0,0 +1,10 @@
+import { PayloadCreateKejiwaanVO, PayloadActivationVO } from "../../../Types"
+
+export interface KejiwaanModel {
+    createData(payload: PayloadCreateKejiwaanVO): Promise<any>
+    updateData(payload: PayloadCreateKejiwaanVO): Promise<any>
+    activation(payload: PayloadActivationVO): Promise<any>
+    deleteData(secureId: string): Promise<any>
+    findOne(secureId: string): Promise<any>
+    findAll(): Promise<any>
+}
